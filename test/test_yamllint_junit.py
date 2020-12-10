4c3b5e159db7ac890dc9e9d915f7cef2052a41a4
Piotr Boruc <wasilp01@stepstone.com>
2020-12-10 10:28:06 +0100

    switched to tox and removed failing test

--- test/test_yamllint_junit.py
+++ test/test_yamllint_junit.py
@@ -2,22 +2,7 @@
 #  -*- coding: utf-8 -*-
 import io
 import mock
-import pytest
 import yamllint_junit_bootstrap.bootstrap as yamllint_junit
-
-
-def test_no_parameters_no_input():
-    # Prepare
-    # It's terrible to mock sys.stdin.fileno() in a test, so I extracted it in an extra method
-    yamllint_junit.is_pipe = mock.MagicMock(return_value=False)
-
-    # Execute
-    with pytest.raises(SystemExit) as pytest_wrapped_e:
-        yamllint_junit.main()
-
-    # Test
-    assert pytest_wrapped_e.type == SystemExit
-    assert pytest_wrapped_e.value.code == 2
 
 
 def test_with_input(monkeypatch):
